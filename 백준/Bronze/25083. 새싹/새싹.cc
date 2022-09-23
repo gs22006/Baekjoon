3e474f3e405e7f092f0b26cd6ae6ac2d58735875
denniskim <108223047+gs22006@users.noreply.github.com>
2022-09-23 14:17:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+
+int main(void)
+{
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	
+	return 0;
+}
