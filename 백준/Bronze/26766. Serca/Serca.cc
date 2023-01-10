6aadf9acea63becf6a7beaeb47d86c4f3f50bac0
denniskim <108223047+gs22006@users.noreply.github.com>
2023-01-11 03:50:07 +0900

    [Bronze V] Title: Serca, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+
+ll n;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	while(n--)
+		cout << " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+	
+	return 0;
+}
