268e7fbf890e499a1b055d23afa0bbb573c2a9c1
denniskim <108223047+gs22006@users.noreply.github.com>
2023-01-31 22:25:58 +0900

    [Platinum IV] Title: 단절점, Time: 48 ms, Memory: 7584 KB -BaekjoonHub

--- 백준/Platinum/11266. 단절점/단절점.cc
+++ 백준/Platinum/11266. 단절점/단절점.cc
@@ -1,71 +1,114 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef int ll;
+typedef __int128 lll;
+typedef long double ld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define sp << " "
+#define en << "\n"
 
-int v, e;
-int all, bll;
-vector<int> vec[10010];
-int chk[10010], num;
-int dan[10010];
-int cou;
+ll n, m;
+ll all, bll;
+vector<ll> vec[10010];
+ll dfsn[10010];
+stack< pair<ll, ll> > stk;
+vector< vector< pair<ll, ll> > > BCC;
+ll cc;
+ll cou[10010];
+vector<ll> ans;
 
-int dfs(int here, int r)
+ll dfs(ll here, ll pa)
 {
-	chk[here] = ++num;
+	ll ret = dfsn[here] = ++cc;
 	
-	int ch = 0;
-	int ret = chk[here];
-	
-	for(int i = 0 ; i < vec[here].size() ; i++)
+	for(auto &i : vec[here])
 	{
-		if(chk[vec[here][i]])
-		{
-			ret = min(ret, chk[vec[here][i]]);
+		if(i == pa)
 			continue;
+		
+		if(dfsn[here] > dfsn[i])
+			stk.push({here, i});
+		
+		if(dfsn[i] > 0)
+			ret = min(ret, dfsn[i]);
+		
+		else
+		{
+			ll tmp = dfs(i, here);
+			
+			ret = min(ret, tmp);
+			
+			if(tmp >= dfsn[here])
+			{
+				vector< pair<ll, ll> > ttmp;
+				pair<ll, ll> hmm = {here, i};
+				
+				while(!stk.empty() && stk.top() != hmm)
+				{
+					ttmp.push_back(stk.top());
+					stk.pop();
+				}
+				
+				ttmp.push_back(stk.top());
+				stk.pop();
+				BCC.push_back(ttmp);
+			}
 		}
-		
-		ch++;
-		int pp = dfs(vec[here][i], 0);
-		
-		if(r == 0 && pp >= chk[here])
-			dan[here] = 1;
-		
-		ret = min(ret, pp);
 	}
-	
-	if(r == 1 && ch >= 2)
-		dan[here] = 1;
 	
 	return ret;
 }
 
 int main(void)
 {
-	scanf("%d %d", &v, &e);
+	fastio
 	
-	for(int i = 0 ; i < e ; i++)
+	cin >> n >> m;
+	
+	for(ll i = 1 ; i <= m ; i++)
 	{
-		scanf("%d %d", &all, &bll);
+		cin >> all >> bll;
+		
 		vec[all].push_back(bll);
 		vec[bll].push_back(all);
 	}
 	
-	for(int i = 1 ; i <= v ; i++)
+	for(ll i = 1 ; i <= n ; i++)
 	{
-		if(chk[i] == 0)
-			dfs(i, 1);
+		if(!dfsn[i])
+			dfs(i, 0);
 	}
 	
-	for(int i = 1 ; i <= v ; i++)
-		cou += dan[i];
-	
-	printf("%d\n", cou);
-	
-	for(int i = 1 ; i <= v ; i++)
+	for(auto &i : BCC)
 	{
-		if(dan[i])
-			printf("%d ", i);
+		set<ll> st;
+		
+		for(auto &j : i)
+		{
+			st.insert(j.fi);
+			st.insert(j.se);
+		}
+		
+		for(auto &j : st)
+			cou[j]++;
 	}
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		if(cou[i] >= 2)
+			ans.push_back(i);
+	}
+	
+	cout << (ll)ans.size() en;
+	
+	for(auto &i : ans)
+		cout << i sp;
 	
 	return 0;
 }
