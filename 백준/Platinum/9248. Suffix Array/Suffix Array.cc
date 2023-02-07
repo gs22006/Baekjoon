9adab37aaed3f99ca1d35fb1d207aad25b33064f
denniskim <108223047+gs22006@users.noreply.github.com>
2023-02-07 18:51:36 +0900

    [Platinum III] Title: Suffix Array, Time: 340 ms, Memory: 82100 KB -BaekjoonHub

--- 백준/Platinum/9248. Suffix Array/Suffix Array.cc
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
+typedef int ll;
 typedef __int128 lll;
 typedef long double ld;
 typedef pair<ll, ll> pll;
@@ -15,105 +15,150 @@ typedef pair<ll, ll> pll;
 #define en << "\n"
 #define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
 
-char s[500010];
-ll n;
-ll SA[500010];
-ll tmp[500010];
-ll ttmp[500010];
-ll LCP[500010];
-ll twoo;
-ll last;
-
-bool cmp(ll X, ll Y)
+struct help
 {
-	if(SA[X] != SA[Y])
-		return SA[X] < SA[Y];
+	ll X, Y, num;
+};
+
+char a[2000010];
+ll n;
+ll grp[2000010];
+ll twoo;
+help SA[2000010];
+ll tmp[2000010];
+ll rdx[2000010];
+help tmp2[2000010];
+ll SISIZ;
+ll LCP[2000010], last;
+
+void do_sort(void)
+{
+	for(ll i = 0 ; i <= SISIZ ; i++)
+		rdx[i] = 0;
 	
-	X += twoo;
-	Y += twoo;
+	for(ll i = 0 ; i < n ; i++)
+		rdx[SA[i].Y]++;
 	
-	if(X >= n || Y >= n)
-		return X > Y;
+	for(ll i = 1 ; i <= SISIZ ; i++)
+		rdx[i] += rdx[i - 1];
 	
-	return SA[X] < SA[Y];
+	for(ll i = n - 1 ; i >= 0 ; i--)
+		tmp2[rdx[SA[i].Y]--] = SA[i];
+	
+	for(ll i = 0 ; i < n ; i++)
+		SA[i] = tmp2[i + 1];
+	
+	for(ll i = 0 ; i <= SISIZ ; i++)
+		rdx[i] = 0;
+	
+	for(ll i = 0 ; i < n ; i++)
+		rdx[SA[i].X]++;
+	
+	for(ll i = 1 ; i <= SISIZ ; i++)
+		rdx[i] += rdx[i - 1];
+	
+	for(ll i = n - 1 ; i >= 0 ; i--)
+		tmp2[rdx[SA[i].X]--] = SA[i];
+	
+	for(ll i = 0 ; i < n ; i++)
+		SA[i] = tmp2[i + 1];
 }
 
 int main(void)
 {
 	fastio
 	
-	cin >> s;
+	cin >> a;
 	
-	n = strlen(s);
+	n = strlen(a);
+	
+	SISIZ = n * 2;
 	
 	for(ll i = 0 ; i < n ; i++)
 	{
-		SA[i] = s[i] - 'a';
-		tmp[i] = i;
+		grp[i] = a[i] - 'a' + 1;
+		SISIZ = max(SISIZ, grp[i] + n);
 	}
 	
-	for(twoo = 1 ; ; (twoo <<= 1))
+	for(twoo = 1 ; ; twoo <<= 1)
 	{
-		sort(tmp, tmp + n, cmp);
-		
-		ll cc = 0;
-		
-		ttmp[tmp[0]] = 0;
-		
-		for(ll i = 1 ; i < n ; i++)
+		for(ll i = 0 ; i < n ; i++)
 		{
-			if(cmp(tmp[i - 1], tmp[i]))
-				cc++;
+			SA[i].X = grp[i];
 			
-			ttmp[tmp[i]] = cc;
+			if(i + twoo >= n)
+				SA[i].Y = -1 + n;
+			else
+				SA[i].Y = grp[i + twoo] + n;
+			
+			SA[i].num = i;
 		}
 		
 		for(ll i = 0 ; i < n ; i++)
-			SA[i] = ttmp[i];
+		{
+			if(SA[i].X > 2000000 || SA[i].Y > 2000000)
+				assert(0);
+		}
+		
+		do_sort();
+		
+		ll cc = 0;
+		
+		tmp[SA[0].num] = 0;
+		
+		for(ll i = 1 ; i < n ; i++)
+		{
+			if(SA[i].X != SA[i - 1].X || SA[i].Y != SA[i - 1].Y)
+				cc++;
+			
+			tmp[SA[i].num] = cc;
+		}
+		
+		for(ll i = 0 ; i < n ; i++)
+			grp[i] = tmp[i];
 		
 		if(cc == n - 1)
 			break;
 	}
 	
 	for(ll i = 0 ; i < n ; i++)
-		SA[i] = tmp[i];
+		cout << SA[i].num + 1 sp;
+	
+	cout en;
 	
 	for(ll i = 0 ; i < n ; i++)
-		tmp[SA[i]] = i;
+		tmp[SA[i].num] = i;
 	
-	for(ll i = 0 ; i < n ; i++)
+	for(ll i = 0 ; i < n ; i++, last = max(0, last - 1))
 	{
+		ll num1 = i, num2 = 0;
+		
 		if(tmp[i] == n - 1)
 		{
-			last = max(last - 1, 0LL);
+			LCP[i] = -1;
 			continue;
 		}
 		
-		ll bun1 = i, bun2 = SA[tmp[i] + 1];
+		num2 = SA[tmp[i] + 1].num;
 		
-		for(ll i = last ; ; i++)
+		for(ll j = last ; ; j++)
 		{
-			if(bun1 + i >= n || bun2 + i >= n)
+			if(num1 + j >= n || num2 + j >= n)
 				break;
 			
-			if(s[bun1 + i] == s[bun2 + i])
+			if(a[num1 + j] == a[num2 + j])
 				last++;
 			else
 				break;
 		}
 		
 		LCP[i] = last;
-		last = max(last - 1, 0LL);
 	}
 	
-	for(ll i = 0 ; i < n ; i++)
-		cout << SA[i] + 1 sp;
-	
-	cout en;
 	cout << "x ";
 	
 	for(ll i = 0 ; i < n - 1 ; i++)
-		cout << LCP[SA[i]] sp;
+		cout << LCP[SA[i].num] sp;
 	
 	return 0;
 }
