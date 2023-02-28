554cc0785cdc330e6e37958a80fbccf0d88f7777
denniskim <108223047+gs22006@users.noreply.github.com>
2023-02-28 23:58:41 +0900

    [Bronze IV] Title: 末尾の文字 (Last Letter), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27541. 末尾の文字 （Last Letter）/末尾の文字 （Last Letter）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+string s;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	cin >> s;
+	
+	if(s.back() == 'G')
+		s.pop_back();
+	else
+		s.push_back('G');
+	
+	cout << s;
+	return 0;
+}
