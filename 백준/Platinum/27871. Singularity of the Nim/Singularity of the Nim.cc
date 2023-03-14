602440230f154371dcc1f7283c63b5a10556ea85
denniskim <108223047+gs22006@users.noreply.github.com>
2023-03-14 17:23:08 +0900

    [Platinum II] Title: Singularity of the Nim, Time: 116 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Platinum/27871. Singularity of the Nim/Singularity of the Nim.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll t;
+ll n, P;
+ll a[200010];
+
+int main(void)
+{
+	fastio
+	
+	cin >> t;
+	
+	while(t--)
+	{
+		cin >> n >> P;
+		
+		for(ll i = 1 ; i <= n ; i++)
+			cin >> a[i];
+		
+		if(a[1] % (P + 1) == 0)
+			cout << "Second\n";
+		else
+			cout << "First\n";
+	}
+	
+	return 0;
+}
