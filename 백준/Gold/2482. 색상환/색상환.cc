39fdc5c98d582d0ff5d302377d4c355379097c53
denniskim <108223047+gs22006@users.noreply.github.com>
2023-03-20 14:11:10 +0900

    [Gold III] Title: 색상환, Time: 8 ms, Memory: 9992 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -1,30 +1,56 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
 
-int n, k;
-long long d[1010][1010];
+ll n, k;
+ll dp[1010][1010];
+ll ss = 1000000003;
+ll ans;
 
 int main(void)
 {
-	scanf("%d %d", &n, &k);
+	fastio
 	
-	if(n / 2 < k)
+	cin >> n;
+	cin >> k;
+	
+	if(k * 2 > n)
 	{
-		printf("0");
+		cout << 0;
 		return 0;
 	}
 	
-	d[0][0] = 1;
+	dp[0][0] = 1;
+	dp[1][0] = dp[1][1] = 1;
 	
-	for(int i = 1 ; i <= n - k + 1 ; i++)
+	for(ll i = 2 ; i <= n ; i++)
 	{
-		for(int j = 0 ; j <= i ; j++)
-			d[i][j] = (d[i - 1][j] + d[i - 1][j - 1]) % 1000000003;
+		for(ll j = 0 ; j <= k ; j++)
+		{
+			dp[i][j] = dp[i - 1][j];
+			
+			if(i - 2 >= 0 && j - 1 >= 0)
+				dp[i][j] = (dp[i][j] + dp[i - 2][j - 1]) % ss;
+		}
 	}
 	
-	long long ans = (d[n - k + 1][k] - d[n - k - 1][k - 2]) % 1000000003;
+	ans = dp[n - 1][k] + dp[n - 3][k - 1];
+	ans = ans % ss < 0 ? ans % ss + ss : ans % ss;
 	
-	printf("%lld", ans < 0 ? ans + 1000000003 : ans);
+	cout << ans;
 	return 0;
 }
