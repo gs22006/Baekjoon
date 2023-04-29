26565f96db261acc6b458a7f158d1899c22ae8ff
denniskim <108223047+gs22006@users.noreply.github.com>
2023-04-30 02:34:18 +0900

    [Gold III] Title: 볼링장 아르바이트, Time: 36 ms, Memory: 5144 KB -BaekjoonHub

---
+++ 백준/Gold/27979. 볼링장 아르바이트/볼링장 아르바이트.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll a[200010];
+ll b[200010];
+ll ans;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	ans = n;
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		cin >> a[i];
+		b[i] = a[i];
+	}
+	
+	sort(b + 1, b + 1 + n);
+	
+	for(ll i = n ; i >= 1 ; i--)
+	{
+		if(a[i] == b[ans])
+			ans--;
+	}
+	
+	cout << ans;
+	return 0;
+}
