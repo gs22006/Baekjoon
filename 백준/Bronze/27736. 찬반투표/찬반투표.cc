cec2301766c1e0a18aa620e0fe15df227c21620e
denniskim <108223047+gs22006@users.noreply.github.com>
2023-05-05 01:48:33 +0900

    [Bronze III] Title: 찬반투표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll a[110];
+ll z, sum;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	for(ll i = 1 ; i <= n ; i++)
+		cin >> a[i];
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		if(!a[i])
+			z++;
+		
+		sum += a[i];
+	}
+	
+	if(z * 2 >= n)
+		cout << "INVALID";
+	else if(sum > 0)
+		cout << "APPROVED";
+	else
+		cout << "REJECTED";
+	
+	return 0;
+}
