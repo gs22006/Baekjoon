e2dc313042abae29c67eb3c06676ad3a03da98fc
denniskim <108223047+gs22006@users.noreply.github.com>
2023-08-01 03:55:25 +0900

    [Bronze IV] Title: Median, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Bronze/9782. Median/Median.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll a[100010];
+ll cc = 1;
+
+int main(void)
+{
+	fastio
+	
+	while(1)
+	{
+		cin >> n;
+		
+		if(!n)
+			break;
+		
+		for(ll i = 1 ; i <= n ; i++)
+			cin >> a[i];
+		
+		if(n & 1)
+			cout << "Case " << cc++ << ": " << a[n / 2 + 1] << ".0\n";
+		
+		else
+		{
+			if((a[n / 2] + a[n / 2 + 1]) & 1)
+				cout << "Case " << cc++ << ": " << (a[n / 2] + a[n / 2 + 1]) / 2 << ".5\n";
+			else
+				cout << "Case " << cc++ << ": " << (a[n / 2] + a[n / 2 + 1]) / 2 << ".0\n";
+		}
+	}
+	
+	return 0;
+}
