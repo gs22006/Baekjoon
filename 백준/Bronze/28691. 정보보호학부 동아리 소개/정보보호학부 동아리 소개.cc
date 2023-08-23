2702b0da11e688a42481cdffb1b26a395eebe9fd
denniskim <108223047+gs22006@users.noreply.github.com>
2023-08-23 22:57:35 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+char s;
+
+int main(void)
+{
+	fastio
+	
+	cin >> s;
+	
+	if(s == 'M')
+		cout << "MatKor";
+	else if(s == 'W')
+		cout << "WiCys";
+	else if(s == 'C')
+		cout << "CyKor";
+	else if(s == 'A')
+		cout << "AlKor";
+	else
+		cout << "$clear";
+	
+	return 0;
+}
