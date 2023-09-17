0020b41049a6c59360708be1547a8831d0afd5e5
denniskim <108223047+gs22006@users.noreply.github.com>
2023-09-18 01:09:52 +0900

    [Bronze III] Title: 최소, 최대, Time: 180 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+int n;
+int a[1000010];
+int minn = 987654321, maxx = -987654321;
+
+int main(void)
+{
+	scanf("%d", &n);
+	
+	for(int i = 0 ; i < n ; i++)
+		scanf("%d", &a[i]);
+	
+	for(int i = 0 ; i < n ; i++)
+	{
+		if(minn > a[i])
+			minn = a[i];
+		
+		if(maxx < a[i])
+			maxx = a[i];
+	}
+	
+	printf("%d %d", minn, maxx);
+	return 0;
+}
