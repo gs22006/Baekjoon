4eede802034096565301ac584254452f816ffedd
denniskim <108223047+gs22006@users.noreply.github.com>
2023-09-18 20:46:59 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -10,12 +10,9 @@ int main(void)
 	
 	for(int i = 1 ; i <= a ; i++)
 	{
-		for(int j = 0 ; j < a - i ; j++)
-			printf(" ");
-		
-		for(int j = 0 ; j < i ; j++)
-			printf("*");
-		
+		for(int j = 1; j <= a; ++j) {
+			printf("%c", j > a - i ? '*' : ' ');
+		}
 		printf("\n");
 	}
 	
