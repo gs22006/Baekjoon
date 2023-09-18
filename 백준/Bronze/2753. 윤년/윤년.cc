13949f2b7567de44ae910f72d462042d07a54cf3
denniskim <108223047+gs22006@users.noreply.github.com>
2023-09-18 20:21:34 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,20 +1,6 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
 using namespace std;
-typedef long long ll;
-typedef __int128 lll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<ld, ld> pld;
-#define MAX 9223372036854775807LL
-#define MIN -9223372036854775807LL
-#define INF 0x3f3f3f3f3f3f3f3f
-#define fi first
-#define se second
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
-#define sp << " "
-#define en << "\n"
-#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
 
 int a;
 
