7686f2636b77e756f0eb627590c99aa5d6b7b176
denniskim <108223047+gs22006@users.noreply.github.com>
2023-09-18 20:43:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -1,30 +1,16 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
 using namespace std;
-typedef long long ll;
-typedef __int128 lll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<ld, ld> pld;
-#define MAX 9223372036854775807LL
-#define MIN -9223372036854775807LL
-#define INF 0x3f3f3f3f3f3f3f3f
-#define fi first
-#define se second
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
-#define sp << " "
-#define en << "\n"
-#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
 
-int n;
+int a;
 
 int main(void)
 {
-	scanf("%d", &n);
+	scanf("%d", &a);
 	
-	for(int i = 1 ; i <= n ; i++)
+	for(int i = 1 ; i <= a ; i++)
 	{
-		for(int j = 0 ; j < n - i ; j++)
+		for(int j = 0 ; j < a - i ; j++)
 			printf(" ");
 		
 		for(int j = 0 ; j < i ; j++)
