f477787eda90d5975e09c1034dfc93f97fc36ab3
denniskim <108223047+gs22006@users.noreply.github.com>
2023-09-18 20:26:10 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -8,16 +8,8 @@ int main(void)
 {
 	scanf("%d", &a);
 	
-	if(a % 4 == 0)
-	{
-		if(a % 100 != 0)
-			printf("1");
-		else if(a % 400 == 0)
-			printf("1");
-		else
-			printf("0");
-	}
-	
+	if(a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+		printf("1");
 	else
 		printf("0");
 	
