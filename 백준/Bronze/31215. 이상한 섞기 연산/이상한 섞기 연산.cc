e964471cf3a5183d7807e491968b0f4cb76a317d
denniskim <108223047+gs22006@users.noreply.github.com>
2024-01-11 04:08:07 +0900

    [Bronze II] Title: 이상한 섞기 연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31215. 이상한 섞기 연산/이상한 섞기 연산.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll t;
+ll n;
+
+int main(void)
+{
+	fastio
+	
+	cin >> t;
+	
+	while(t--)
+	{
+		cin >> n;
+		
+		if(n <= 2)
+		{
+			cout << 1 en;
+			continue;
+		}
+		
+		cout << 3 en;
+	}
+	
+	return 0;
+}
