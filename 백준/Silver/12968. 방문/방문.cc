dda8f55f016538c4fd1d33c736317895c8dc8472
denniskim <108223047+gs22006@users.noreply.github.com>
2024-01-12 03:25:15 +0900

    [Silver I] Title: 방문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12968. 방문/방문.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef int ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 987654321
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n, m, K;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n >> m >> K;
+	
+	if(n * m % 2 == 0)
+		cout << 1;
+	else if(K == 1)
+		cout << 1;
+	else
+		cout << 0;
+	
+	return 0;
+}
