4756e857e123b3b3c61a8b2ee0b3fc2dcbb7eb0a
denniskim <108223047+gs22006@users.noreply.github.com>
2024-02-21 03:05:54 +0900

    [Bronze I] Title: 당직 근무표, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Bronze/31408. 당직 근무표/당직 근무표.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll cou[100010];
+ll ans;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		cin >> cou[0];
+		
+		cou[cou[0]]++;
+		ans = max(ans, cou[cou[0]]);
+	}
+	
+	if(ans > (n + 1) / 2)
+		cout << "NO";
+	else
+		cout << "YES";
+	
+	return 0;
+}
