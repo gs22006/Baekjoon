96797d0959712054ae036d2c5d8d4427d1409609
denniskim <108223047+gs22006@users.noreply.github.com>
2024-03-24 23:17:27 +0900

    [Bronze IV] Title: 뉴턴과 사과, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13118. 뉴턴과 사과/뉴턴과 사과.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll a[10];
+ll x, y, z;
+
+int main(void)
+{
+	fastio
+	
+	for(ll i = 1 ; i <= 4 ; i++)
+		cin >> a[i];
+	
+	cin >> x >> y >> z;
+	
+	for(ll i = 1 ; i <= 4 ; i++)
+	{
+		if(a[i] == x)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	
+	cout << 0;
+	return 0;
+}
