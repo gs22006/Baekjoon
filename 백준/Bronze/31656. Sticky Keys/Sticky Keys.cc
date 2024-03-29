2eb28c6cd87cdbbbeed0f33e102042b7c29dcc10
denniskim <108223047+gs22006@users.noreply.github.com>
2024-03-30 01:22:01 +0900

    [Bronze IV] Title: Sticky Keys, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31656. Sticky Keys/Sticky Keys.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+char a[1010];
+char last;
+
+int main(void)
+{
+	fastio
+	
+	fgets(a, 1010, stdin);
+	
+	for(ll i = 0 ; a[i] != '\0' ; i++)
+	{
+		if(a[i] == last)
+			continue;
+		
+		cout << a[i];
+		last = a[i];
+	}
+	
+	return 0;
+}
