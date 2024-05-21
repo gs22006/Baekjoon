91716ac6281d6c0313cca4ab3aa28c75471a4ffc
denniskim <108223047+gs22006@users.noreply.github.com>
2024-05-21 11:17:33 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 28 ms, Memory: 2308 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n, m;
+ll all, bll, cll;
+vector< pair<ll, pll> > vec;
+ll dp[10010], tmp[10010];
+ll ans;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n >> m;
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		cin >> all >> bll >> cll;
+		
+		ll now = 1;
+		
+		while(1)
+		{
+			if(cll < now)
+				break;
+			
+			vec.push_back({now, {all * now, bll * now}});
+			cll -= now;
+			now <<= 1;
+		}
+		
+		if(cll)
+			vec.push_back({cll, {all * cll, bll * cll}});
+	}
+	
+	for(auto &i : vec)
+	{
+		for(ll j = m ; j >= 0 ; j--)
+		{
+			if(j - i.se.fi >= 0)
+				tmp[j] = max(tmp[j], dp[j - i.se.fi] + i.se.se);
+		}
+		
+		for(ll j = 0 ; j <= m ; j++)
+			dp[j] = tmp[j];
+	}
+	
+	for(ll i = 0 ; i <= m ; i++)
+		ans = max(ans, dp[i]);
+	
+	cout << ans;
+	return 0;
+}
