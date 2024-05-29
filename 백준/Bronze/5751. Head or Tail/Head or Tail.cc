c30b2ec5d4f2ca99f53c180eb4a2ac44ddd8ca02
denniskim <108223047+gs22006@users.noreply.github.com>
2024-05-30 00:01:08 +0900

    [Bronze III] Title: Head or Tail, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5751. Head or Tail/Head or Tail.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll all;
+
+int main(void)
+{
+	fastio
+	
+	while(1)
+	{
+		ll cou[10] = {0, };
+		
+		cin >> n;
+		
+		if(!n)
+			break;
+		
+		for(ll i = 1 ; i <= n ; i++)
+		{
+			cin >> all;
+			cou[all]++;
+		}
+		
+		cout << "Mary won " << cou[0] sp <<  "times and John won " << cou[1] sp << "times\n";
+	}
+	
+	return 0;
+}
