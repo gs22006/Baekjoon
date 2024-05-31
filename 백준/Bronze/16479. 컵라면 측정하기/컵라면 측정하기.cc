48043de53fd46c45d17f11d9c3cb9e6a879a7997
denniskim <108223047+gs22006@users.noreply.github.com>
2024-05-31 12:00:37 +0900

    [Bronze III] Title: 컵라면 측정하기, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll k, d1, d2;
+
+int main(void)
+{
+	fastio
+	
+	cin >> k;
+	cin >> d1 >> d2;
+	cout << k * k - ((d1 - d2) / 2.0) * ((d1 - d2) / 2.0);
+	return 0;
+}
