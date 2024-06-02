dcf5737bcb8263e5c4eadb01f0dcdefb35bfbb16
denniskim <108223047+gs22006@users.noreply.github.com>
2024-06-02 23:45:10 +0900

    [Bronze III] Title: 아이그루스와 화장실, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30642. 아이그루스와 화장실/아이그루스와 화장실.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll K;
+string s;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	cin >> s;
+	cin >> K;
+	
+	if(s[0] == 'a')
+	{
+		for(ll i = -1 ; i <= 1 ; i++)
+		{
+			if(1 <= K + i && K + i <= n && (K + i) % 2 == 1)
+			{
+				cout << K + i;
+				return 0;
+			}
+		}
+	}
+	
+	else
+	{
+		for(ll i = -1 ; i <= 1 ; i++)
+		{
+			if(1 <= K + i && K + i <= n && (K + i) % 2 == 0)
+			{
+				cout << K + i;
+				return 0;
+			}
+		}
+	}
+	
+	return 0;
+}
