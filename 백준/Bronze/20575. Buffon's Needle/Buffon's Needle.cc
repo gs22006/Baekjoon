4062d213af1c7af5a83bf4d41e3481f6f77896bc
denniskim <108223047+gs22006@users.noreply.github.com>
2024-06-05 10:09:50 +0900

    [Bronze I] Title: Buffon's Needle, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20575. Buffon's Needle/Buffon's Needle.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ld all, bll, cll, dll;
+ld ans;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	for(ll i = 1 ; i <= n ; i++)
+	{
+		cin >> all >> bll >> cll >> dll;
+		
+		if(all > cll)
+			swap(all, cll);
+		
+		for(ll j = all - 2 ; j <= cll + 2 ; j++)
+		{
+			if(all <= j && j <= cll)
+			{
+				ans++;
+				break;
+			}
+		}
+	}
+	
+	ans = 2.0 / (ans / (ld)n);
+	
+	cout << ans;
+	return 0;
+}
