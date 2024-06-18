b332cc61f1ae6a04971afe76805b0c6b485da03e
denniskim <108223047+gs22006@users.noreply.github.com>
2024-06-19 00:45:18 +0900

    [Bronze IV] Title: 강당 대관, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/31994. 강당 대관/강당 대관.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+string s;
+ll all;
+set< pair<ll, string> > st;
+
+int main(void)
+{
+	fastio
+	
+	for(ll i = 1 ; i <= 7 ; i++)
+	{
+		cin >> s >> all;
+		st.insert({all, s});
+	}
+	
+	cout << (*st.rbegin()).se;
+	return 0;
+}
