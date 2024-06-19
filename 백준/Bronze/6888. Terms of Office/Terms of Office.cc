2ebef1bc29a9f6cd37045b1d023faa3e8bba788b
denniskim <108223047+gs22006@users.noreply.github.com>
2024-06-19 13:06:45 +0900

    [Bronze IV] Title: Terms of Office, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll a, b;
+
+int main(void)
+{
+	fastio
+	
+	cin >> a;
+	cin >> b;
+	
+	for(ll i = a ; i <= b ; i += 60)
+		cout << "All positions change in year " << i en;
+	
+	return 0;
+}
