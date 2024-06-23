5ce8d2768c7609ce718251ea8069c7ad9d63e6f5
denniskim <108223047+gs22006@users.noreply.github.com>
2024-06-23 21:48:44 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll a, b, c;
+
+int main(void)
+{
+	fastio
+	
+	cin >> a >> b >> c;
+	cout << max(a + c, b);
+	return 0;
+}
