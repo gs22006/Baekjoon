207ad7c89acce4f471ee2832b5c2051f83ab466e
denniskim <108223047+gs22006@users.noreply.github.com>
2024-08-10 06:41:04 +0900

    [Gold IV] Title: 중복 제거, Time: 640 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Gold/13701. 중복 제거/중복 제거.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef int ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+bitset<(1<<25)> bt;
+ll all;
+
+int main(void)
+{
+	fastio
+	
+	while(cin >> all)
+	{
+		if(bt[all])
+			continue;
+		
+		cout << all sp;
+		bt[all] = 1;
+	}
+	
+	return 0;
+}
