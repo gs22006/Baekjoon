07137d19fd268e4c6e4cfba294767624679510c4
denniskim <108223047+gs22006@users.noreply.github.com>
2024-08-26 22:40:40 +0900

    [Silver III] Title: 서바이벌, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32142. 서바이벌/서바이벌.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+#define MAX 9223372036854775807LL
+#define MIN -9223372036854775807LL
+#define INF 0x3f3f3f3f3f3f3f3f
+#define fi first
+#define se second
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout << fixed; cout.precision(10);
+#define sp << " "
+#define en << "\n"
+#define compress(v) sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), v.end())
+
+ll n;
+ll all, bll;
+
+int main(void)
+{
+	fastio
+	
+	cin >> n;
+	
+	while(n--)
+		cin >> all >> bll;
+	
+	cout << -1;
+	return 0;
+}
